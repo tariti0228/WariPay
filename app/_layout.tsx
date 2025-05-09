829309b262c80133e347ef3202860b723e4f27dc
tariti0228 <tariti0228@gmail.com>
2025-05-09 04:06:44 +0000

    tab適用

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -35,8 +35,8 @@ export default function RootLayout() {
   return (
     <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
       <PaperProvider theme={theme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
           <Stack.Screen name="+not-found" options={{ title: 'ページが見つかりません' }} />
         </Stack>
       </PaperProvider>
